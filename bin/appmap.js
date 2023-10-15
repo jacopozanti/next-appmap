1d6dbd17303ca6b2e88ed595393d335350187355
Jacopo Zanti <jacopoz99@gmail.com>
2023-10-15 07:47:48 +0200

    Locally call method

--- bin/appmap.js
+++ bin/appmap.js
@@ -1,8 +1,16 @@
 #! /usr/bin/env node
+
 //var shell = require("shelljs");
 // shell.exec("pnpm help"); // Execute things on shell
 
 const dirTree = require("directory-tree");
-const tree = dirTree(".");
+const tree = dirTree(
+  ".", 
+  {
+    attributes: ["type", "extension"],
+    extensions: /\.(js|ts|tsx)$/,
+    exclude: [/node_modules/, /.git/],
+  }
+);
 
 console.log(tree);
