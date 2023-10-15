3c905adee6789ee7d525829962085a85184fbac0
Jacopo Zanti <jacopoz99@gmail.com>
2023-10-15 07:06:00 +0200

    Cli test

---
+++ bin/start.js
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+//var shell = require("shelljs");
+// shell.exec("pnpm help"); // Execute things on shell
+
+const dirTree = require("directory-tree");
+const tree = dirTree("../../");
+
+console.log(tree);
